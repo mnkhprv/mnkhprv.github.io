39ca54c3dc8f2299e1da2f97b97a8e460f0d4537
munkhprv <munkhpurev6@gmail.com>
2024-12-15 03:31:48 +0800

    Lab-9

---
+++ scripts/word-list.js
@@ -0,0 +1,10 @@
+const wordList = [
+  {  
+    word: "guitar",
+    hint: "A musical instrument with strings."
+  },
+  {
+    word: "astronomy",
+    hint: "the scientific wtudy of celestial objects and phenomena"
+  }
+]
