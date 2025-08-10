4926482c8ff43a647ea1f7e6efc1c5b91c8cc736
mnkhprv <munkhpurev122@gmail.com>
2025-08-10 16:23:33 +0800

    j

--- script.js
+++
@@ -1,8 +0,0 @@
-const keyboardDiv = document.querySelector(".keyboard");
-
-for (let i = 97; i <= 122; i++) {
-    const button = document.createElement("button");
-    button.innerText = String.fromCharCode(i);
-    keyboardDiv.appendChild(button);
-    
-}
